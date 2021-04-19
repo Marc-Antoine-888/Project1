14438fcc9c971dab8847e2ffe03c4e5e6e2d6802
Marc-Antoine-888 <78393420+Marc-Antoine-888@users.noreply.github.com>
2021-04-19 17:50:24 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -29,3 +29,34 @@ public class Main {
     }
 
 }
+
+public class Livre {
+
+    private String titre;
+    private String auteur;
+    private double prix;
+
+    private static int nbLivres;
+
+    public Livre(String titre, String auteur, double prix) {
+        this.titre = titre;
+        this.auteur = auteur;
+        this.prix = prix;
+        nbLivres++;
+    }
+
+    public static int getNbLivres() {
+        return nbLivres;
+    }
+
+    public static void setNbLivres(int nbLivres) {
+        Livre.nbLivres = nbLivres;
+    }
+
+    @Override
+    public String toString() {
+        return titre + ", " + auteur + ", " + prix;
+    }
+}
+
+
